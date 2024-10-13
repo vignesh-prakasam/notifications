8cdd6f71079f54764bf169f4f6106babb72aa642
Vignesh Prakasam <vigneshp.ceg@gmail.com>
2024-10-13 12:21:33 +0530

    layout done

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,50 @@
 import { useState } from "react";
 import "./App.css";
+import Message from "./Message";
+import avatarMarkWebber from './assets/images/avatar-mark-webber.webp';
+import avatarAngelaGray from './assets/images/avatar-angela-gray.webp';
+import avatarJacobThompson from './assets/images/avatar-jacob-thompson.webp';
+import avatarRizkyHasanuddin from './assets/images/avatar-rizky-hasanuddin.webp';
+import avatarKimberlySmith from './assets/images/avatar-kimberly-smith.webp';
+import avatarNathanPeterson from './assets/images/avatar-nathan-peterson.webp';
+import avatarAnnaKim from './assets/images/avatar-anna-kim.webp';
+import chessImage from './assets/images/image-chess.webp'
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [messages, setMessages] = useState(0);
 
   return (
-    <>
+    <main>
       <div className="flex flex-col min-h-screen justify-center items-center">
-        <div className="flex flex-col bg-white w-2/3 h-96 rounded-lg">
+        <div className="flex flex-col bg-white w-2/3 h-full rounded-lg">
           <div className="flex justify-between mt-2 mx-3">
-            <div><h1 className="text-left font-bold text-xl">Notifications <span className="size-10 bg-neutral-very-dark-blue ml-2 py-1 px-2 rounded text-white text-sm font-semibold"> 3 </span></h1></div>
-            <h1 className="text-left text-neutral-grayish-blue text-sm">Mark all read</h1>
+            <div>
+              <h1 className="text-left font-bold text-xl">
+                Notifications{" "}
+                <span className="size-10 bg-primary-blue ml-2 py-1 px-2 rounded text-white text-sm font-semibold">
+                  {" "}
+                  3{" "}
+                </span>
+              </h1>
+            </div>
+            <a className="text-left text-neutral-grayish-blue text-sm" href="#">
+              Mark all read
+            </a>
           </div>
+          {/* messages */}
+          <div className="mx-5">
+            <Message avatar={avatarMarkWebber} name="Mark Webber" entity="My first tournament today" event="reacted to your post" timeAgo={"1m ago"} />
+            <Message avatar={avatarAngelaGray} name="Angela Gray" entity="" event="followed you" timeAgo={"5m ago"}/>
+            <Message avatar={avatarJacobThompson} name="Jacob Thompson" entity="Chess Club" event="has joined your group" timeAgo={"1 day ago"}/>
+            <Message avatar={avatarRizkyHasanuddin} name="Rizky  Hasanuddin" entity="" event="sent you a private message" message="Hello, thanks for setting up the Chess Club. I've been a member for a few weeks now and I'm already having lots of fun and improving my game." timeAgo={"5 days ago"}/>
+            <Message avatar={avatarKimberlySmith} name="Kimberly Smith" entity="" event="commented on your picture" picture={chessImage} timeAgo={"1 week ago"}/>
+            <Message avatar={avatarNathanPeterson} name="Nathan Peterson" entity="5 end-game strategies to increase your win rate" event="reacted to your recent post" timeAgo={"2 weeks ago"}/>
+            <Message avatar={avatarAnnaKim} name="Anna Kim" entity="Chess Club" event="left the group" timeAgo={"2 weeks ago"}/>
+          </div>
+         
         </div>
       </div>
-    </>
+    </main>
   );
 }
 
---
+++ src/Message.jsx
@@ -0,0 +1,34 @@
+
+
+
+export default function Message({avatar, name, entity, event, message, picture, timeAgo, status}) {
+    return (
+        <div className="grid sm:grid-cols-9 grid-cols-5 gap-4 bg-neutral-light-grayish-blue-2 rounded-lg my-2">
+            <div className="sm:col-span-1 col-span-1">
+                <img src={avatar} alt="avatar" className="rounded-full size-12 m-4" />
+            </div>
+            <div className="sm:col-span-8 col-span-4 mx-2">
+                <div className="mt-5 text-left">
+                    <div className="flex flex-row justify-between">
+                                <div>
+                                    <strong className="text-sm font-bold">{name}</strong> 
+                                    <span className="text-sm text-neutral-dark-grayish-blue"> {event} </span>
+                                    <a href="#" className="text-primary-blue font-bold text-sm">{entity}</a>
+                                    <span className="inline-block w-2 h-2 bg-red-500 rounded-full ml-1"></span>
+                                    <p className="text-neutral-grayish-blue text-sm text-left ">{timeAgo}</p>
+                                </div>
+                                {
+                                    picture && <img src={picture} alt="picture" className="rounded-lg size-12 mt-1 mr-5" />
+                                }
+                    </div>
+                    
+                </div>
+                
+                <div className="rounded-lg border-10 border-black">
+                    <p className="p-3 text-neutral-dark-grayish-blue text-sm  text-left">{message}</p>
+                </div>
+               
+            </div>
+        </div>
+    );
+}
